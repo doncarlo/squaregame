6f24372fd8bec070d9217f3d8df9a398df5f63f9
don_carlo <doncarlo@github.com>
2019-02-25 22:42:21 -0800

    Added basic functionality. Can click on sides, and all connected users will receive click events

---
+++ squaregame/Hubs/GameHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SquareGame.Hubs
+{
+    public class GameHub : Hub
+    {
+        public async Task ClickSide(string player, int x, int y, int i)
+        {
+            await Clients.Others.SendAsync("UpdateBoard", player, x, y, i);
+        }
+    }
+}
--- squaregame/Startup.cs
+++ squaregame/Startup.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using SquareGame.Hubs;
 
 namespace squaregame
 {
@@ -25,7 +20,8 @@ namespace squaregame
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,13 +31,17 @@ namespace squaregame
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseHsts();
-            }
+
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<GameHub>("/gameHub");
+            });
+            //app.UseMvc();
         }
     }
 }
